2a751d2ca8fff165ef6403fb5246e1edcb02142f
sashasdsd <168480668+sashasdsd@users.noreply.github.com>
2025-02-11 22:03:28 +0200

    Delete Валентинка directory

--- Валентинка/js/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("btn");
-const body = document.getElementById("body");
-const key = document.getElementById("lock");
-function Unlock() {
-	key.classList.add('active');
-	body.classList.add('active');
-}
